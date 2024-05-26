b2653177846e0562cadc2741731b9b77ee0c75e1
shreyagr309 <shreyagrawal499@gmail.com>
2024-05-25 21:50:30 -0600

    Update build.gradle.kts
    Changed the compileSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.memorypalaceapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.memorypalaceapp"
